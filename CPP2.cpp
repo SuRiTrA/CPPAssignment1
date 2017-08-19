fd1c5c515f454448885fd96a84ba1a52dd344163
SurisGithub <31152879+SurisGithub@users.noreply.github.com>
2017-08-19 13:15:48 +0530

    Create CPP2.cpp

---
+++ CPP2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*C++ program to accept two numbers and print their sum.*/
+
+int main()
+{ 
+  float a=0,b=0;
+  float sum;
+  cout<<"Hello there, Welcome"<<endl;
+  cout<<"\n \nEnter the first number: ";
+  cin>>a;
+  cout<<"\nEnter the second number: ";
+  cin>>b;
+  sum=a+b;
+  cout<<"\n \nThe Sum of the two numbers entered is: "<<sum<<endl;
+  return 0;
+}
